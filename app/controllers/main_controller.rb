55981f03204c8d92067c8396b2e7d2d0ab24e7d6
Karl Turner <karl.turner5@gmail.com>
2012-12-23 18:00:07 +0000

    updated searches to return images

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,8 +6,10 @@ class MainController < ApplicationController
   end
 
   def results
-    @results = movie_search(params[:search])
-    
+    conf = image_config_settings
+    results = movie_search(params[:search])
+    @title = results.first["title"]
+    @image = conf["base_url"] + conf["size"] + results.first["image"]
   end
 
 end
--- lib/movie/movie_finder.rb
+++ lib/movie/movie_finder.rb
@@ -1,12 +1,22 @@
 module Movie::MovieFinder
 
-URL = "http://api.themoviedb.org/3/search/movie?api_key="
+MOVIE_URL = "http://api.themoviedb.org/3/search/movie?api_key="
+CONFIG_URL = "http://api.themoviedb.org/3/configuration?api_key="
 API_KEY = APP_CONFIG['api_key']
 
   def movie_search(query)
-    response = HTTParty.get(URL + API_KEY + "&query=#{query}", :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
+    response = HTTParty.get(MOVIE_URL + API_KEY + "&query=#{query}", :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
     results = JSON.parse(response.body)["results"]
     clean_up(results)
+  end
+
+  def image_config_settings
+    response = HTTParty.get(CONFIG_URL + API_KEY, :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
+    results = JSON.parse(response.body)["images"]
+    {
+      "base_url" => results["base_url"],
+      "size" => results["poster_sizes"].last,
+    }
   end
 
 private
