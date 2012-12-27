2e966576f4a707785fd6cf5362ef3e404d3d0814
otaiga <karl.turner5@gmail.com>
2012-12-27 09:08:54 +0000

    adding correct pem for bootstrap production

--- config/provision/bootstrap/bootstrap_production.sh
+++ config/provision/bootstrap/bootstrap_production.sh
@@ -5,10 +5,10 @@ host="${1:-ubuntu@ec2-54-247-32-142.eu-west-1.compute.amazonaws.com}"
 ssh-keygen -R "${host#*@}" 2> /dev/null
 
 echo "****** SCP'ing to $host ..."
-scp -i ~/. -r config/provision $host:/home/vagrant/
+scp -i ~/.ssh/movieposter.pem -r config/provision $host:/home/vagrant/
 
 echo "****** Running setup-prereq.sh..."
-ssh -i ~/. "$host" '
+ssh -i ~/.ssh/movieposter.pem "$host" '
 . $HOME/provision/bootstrap/setup-prereq.sh'
 
 echo "****** Package install complete..."
