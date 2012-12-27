8525d2bd07ab5adbc16c154fd6cabcae6963ce31
otaiga <karl.turner5@gmail.com>
2012-12-27 14:11:21 +0000

    added check for search params - could be better

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,10 +7,14 @@ class MainController < ApplicationController
   end
 
   def results
-    conf = image_config_settings
-    results = movie_search(params[:search]) 
-    @results = removed_nil_images(results).paginate(:page => params[:page], :per_page => 1)  
-    @image = conf["base_url"] + conf["size"]
+    if !params[:search].empty?
+      conf = image_config_settings
+      results = movie_search(params[:search])
+      @results = removed_nil_images(results).paginate(:page => params[:page], :per_page => 1)
+      @image = conf["base_url"] + conf["size"]
+    else
+      redirect_to root_path
+    end
   end
 
 private
