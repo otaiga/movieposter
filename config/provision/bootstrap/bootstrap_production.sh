66022a6a7da97157fa4f2a67b65b97b70e9f05cd
otaiga <karl.turner5@gmail.com>
2012-12-27 09:12:13 +0000

    adding correct user for bootstrap production

--- config/provision/bootstrap/bootstrap_production.sh
+++ config/provision/bootstrap/bootstrap_production.sh
@@ -5,7 +5,7 @@ host="${1:-ubuntu@ec2-54-247-32-142.eu-west-1.compute.amazonaws.com}"
 ssh-keygen -R "${host#*@}" 2> /dev/null
 
 echo "****** SCP'ing to $host ..."
-scp -i ~/.ssh/movieposter.pem -r config/provision $host:/home/vagrant/
+scp -i ~/.ssh/movieposter.pem -r config/provision $host:/home/ubuntu/
 
 echo "****** Running setup-prereq.sh..."
 ssh -i ~/.ssh/movieposter.pem "$host" '
