3880be0b1be4914ba888efde27873596705b41eb
otaiga <karl.turner5@gmail.com>
2012-12-27 13:12:14 +0000

    oops missed the definition in the site

--- config/provision/puppet/manifests/site.pp
+++ config/provision/puppet/manifests/site.pp
@@ -10,5 +10,6 @@ $base_dir       = extlookup("base_dir"       , "/var/apps/")
 $app_dir        = extlookup("app_dir"        , "/var/apps/my_app_name")
 $passenger_version = extlookup("passenger_version"        , "3.0.18")
 $pem_pub = extlookup("pem_pub")
+$app_url = extlookup("app_url")
 
 import "nodes.pp"
