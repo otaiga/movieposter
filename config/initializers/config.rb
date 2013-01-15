f1446768eb53fb51acd1795fa6ba04dedb0953bd
otaiga <karl.turner5@gmail.com>
2013-01-15 14:20:23 +0000

    told you it wouldn't work

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,1 +1,7 @@
-APP_CONFIG = lambda { begin;YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env];rescue}
+APP_CONFIG = lambda do
+  begin
+    YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+  rescue
+    #nothing
+  end
+end
